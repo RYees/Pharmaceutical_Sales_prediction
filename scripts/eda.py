5c1f89cba81437315cd6ca2040be48663ef789a9
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-05-24 08:43:17 +0300

    exploring data

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-import warnings
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import warnings
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import sys
 
 
@@ -16,7 +16,7 @@ class EDA:
         """initialize the eda class"""
         self.df = df
 
-    def descriptive_stats(self, describe=False, info=False, size=False):
+    def data_describe(self, describe=False, info=False, size=False):
         """
         expects: 
             - boolean
@@ -26,8 +26,8 @@ class EDA:
         summary = None
         if describe:
             summary = self.df.describe()
-        elif info:
-            summary = self.df.info
+        # elif info:
+            # summary = self.df.head(2)
         elif size:
             summary = self.df.shape
         return summary
@@ -44,39 +44,6 @@ class EDA:
             has_missing_values = True
         return has_missing_values
 
-    # def plot_counts(self, column, second_column=None, type=None):
-    #     """
-    #     expects:
-    #         -   string
-    #     returns:
-    #         -   plot
-    #     """
-    #     if type == "univariate":
-    #         plt.figure(figsize=(12, 6))
-    #         sns.countplot(data=self.df, x=column)
-    #         plt.title(f"Unique value counts of the {column} columns")
-    #         plt.show()
-    #     elif type == "bivariate":
-    #         plt.figure(figsize=(12, 6))
-    #         sns.countplot(data=self.df, x=second_column, hue=column)
-    #         plt.title(f"{column} vs {second_column}")
-    #         plt.show()
-    #     return
-
-    # def correlation_analysis(self):
-    #     """
-    #     expects:
-    #         - nothing
-    #     returns:
-    #         - dataframe
-    #     """
-    #     corr = self.df.corr()
-    #     fig, ax = plt.subplots()
-    #     sns.heatmap(corr, annot=True)
-    #     plt.title('Heatmap of correlation for the numerical columns')
-    #     plt.show()
-    #     return fig
-
     def get_df(self):
         """
         - returns the dataframes
@@ -88,5 +55,5 @@ if __name__ == '__main__':
     file_path = sys.argv[1]
     df = pd.read_csv(file_path)
     eda = EDA(df)
-    eda_df = eda.get_df()
-    eda_df.to_csv("data/eda.csv", index=False)
+    # eda_df = eda.get_df()
+    # eda_df.to_csv("data/eda.csv", index=False)
